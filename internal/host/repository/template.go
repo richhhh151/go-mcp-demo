1fdcdc5a802129d494ead01c4b5148f8cebd4442
Superdarling <2857043800@qq.com>
2025-11-25 13:12:00 +0800

    Update template.go

--- internal/host/repository/template.go
+++ internal/host/repository/template.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"github.com/FantasyRL/go-mcp-demo/pkg/gorm-gen/model"
+	"github.com/openai/openai-go/v2"
 )
 
 // TemplateRepository 根据实际需求定义上层访问的接口，在下层infra做具体方法的实现
@@ -11,4 +12,8 @@ type TemplateRepository interface {
 	CreateUserByIDAndName(ctx context.Context, id string, name string) (*model.Users, error)
 	// GetUserByID 通过ID获取用户信息
 	GetUserByID(ctx context.Context, id string) (*model.Users, error)
+	// UpsertConversation 插入或更新对话记录
+	UpsertConversation(ctx context.Context, userID string, conversationID string, openaiMessages []openai.ChatCompletionMessageParamUnion) error
+	// GetConversationByID 通过ID获取对话记录
+	GetConversationByID(ctx context.Context, id string) (*model.Conversations, error)
 }
